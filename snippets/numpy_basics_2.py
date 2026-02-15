ffe741dbd22cede62ed286b297a00f6a96aeff01
Bhargava Ram <bhargavram466@gmail.com>
2026-02-15 13:45:44 -0800

    Add basic NumPy operations in numpy_basics_2.py

---
+++ snippets/numpy_basics_2.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4, 5])
+
+print("Original:" , a)
+
+print("Add 10:", a + 10)
+print("Multiply by 2:", a * 2)
+print("squared:", a ** 2)
+
+print("Mean:", np.mean(a))
+print("Sum:", np.sum(a))
+print("Standard Deviation:", np.std(a))
+
